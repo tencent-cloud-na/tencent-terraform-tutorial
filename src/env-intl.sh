f0b7958d67b2dd4620cf1866574b25eefc648a8d
Jianhe Liao <jliao@zhl-tech.com>
2021-12-26 18:41:52 -0800

    Remove terraform state files

--- src/env-intl.sh
+++ src/env-intl.sh
@@ -1,5 +1,5 @@
 # Environment variables for Terraform script
-export TENCENTCLOUD_SECRET_ID="IKIDSjmbCYgZEftypSs3zgCTVJtH3bv2va1F"
-export TENCENTCLOUD_SECRET_KEY="4tUr0uYw4ze4FArKk14uWv50g7IEuKVQ"
+export TENCENTCLOUD_SECRET_ID="IKIDSjmbCYxxxxxxxxxxxxxxx"
+export TENCENTCLOUD_SECRET_KEY="4tUr0uYw4zyyyyyyyyyyyyyyy"
 export TF_LOG=DEBUG
-export TF_LOG_PATH=/Users/jliao/src/tencent-modules/logs/terraform.log
+export TF_LOG_PATH=/Users/somebody/src/tencent-modules/logs/terraform.log
